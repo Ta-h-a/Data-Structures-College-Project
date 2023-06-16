a3fa0a0faf1cf4783f1e5ea136c22a2af43ed8d6
Ta-h-a <tahasindoli@gmail.com>
2023-06-16 15:36:46 +0530

    Changes made 03

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const _ = require("lodash");
 const app = express();
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
